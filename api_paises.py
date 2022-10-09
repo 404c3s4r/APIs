4c73bc944f6ec29187cacb6acd148b30ddee0ccf
Roberto Oliveira <robertocoliver@protonmail.com>
2022-10-08 21:25:27 -0300

    Update api_paises.py

--- api_paises.py
+++ api_paises.py
@@ -1,10 +1,13 @@
 import requests
 import json
 
-def requisicao_HTTP_JSON(dress):
+
+url = 'https://restcountries.com/v3.1/all'
+
+def requisicao_HTTP_JSON(url):
     try:
 
-        requisicao = requests.get(dress)
+        requisicao = requests.get(url)
 
         if requisicao.status_code == 200:
             return requisicao.text
@@ -26,47 +29,23 @@ def contagem(all_paises):
 	return len(all_paises)
 
 
-def listagem_paises(listagem_paises):
-	for cada in listagem_paises:
-		print(cada['name'])
+def listagem_paises(listagem):
+	for cada in listagem:
+		print(cada['name']['common'])
 
-def populacao(pais="Brazil"):
-	paises = requisicao_HTTP_JSON(f"https://restcountries.com/v3.1/name//{pais}")
-
-	if paises:
-		parsing_pais = parsing(paises)
-	if parsing_pais:
-		for pais in parsing_pais:
-			print(f"{pais['name']}: {pais['population']}")
-	else:
-		print("pais não encontrado")
-
-def moedas(pais="Brazil"):
-	moedas = requisicao_HTTP_JSON(f"https://restcountries.com/v3.1/name//{pais}")
-	if moedas:
-		parsing_moedas = parsing(moedas)
-		if parsing_moedas:
-			for cada in parsing_moedas:
-				print(f"{pais['name']}: {pais['currencies']}")
-		else:
-			print("Pais não encontrado")
-
-
-
-
-url = input("Digite uma url: ")
-
-
-
-if __name__ == "__main__":
-	objeto_python = requisicao_HTTP_JSON(url)
-	if objeto_python:
-		done_parsing = parsing(objeto_python)
-
-		if done_parsing:
-			listagem_paises(done_parsing)
-			print(contagem(done_parsing))
-while True:
-	pais_select = input("Escolha um pais: ")
-	populacao(pais_select)
-	moedas(pais_select)
+def populacao(pais):
+   
+    paises = requisicao_HTTP_JSON(f'https://restcountries.com/v3.1/name//{pais}')
+    p = parsing(paises)
+    for cada in p:
+        print(f"{cada['population']}")
+        
+if __name__=='__main__':
+    texto = requisicao_HTTP_JSON(url)
+    ParsingError = parsing(texto)
+    if parsing:
+        listagem_paises(parsing)
+    
+    while True:
+        pais = input('país: ')
+        populacao(pais)
